9786f58a550a28608f98090b5861a174d91dd4c7
thiagojose <113394925+thigodev@users.noreply.github.com>
2024-10-05 23:55:18 -0300

    Update file .sql by Thiago

--- sql/configure.sql
+++ sql/configure.sql
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `umbrella1`
+-- Banco de dados: `configure`
 --
 
 -- --------------------------------------------------------
