739eaa0ea2dc7b40fe25be0af8763d3e1de8a33e
evilyn-araujo <evilyndeveloper@gmail.com>
2024-11-20 08:40:14 -0300

    eu adicionei uma funcionalidade de ocultar a senha e apresentar na tela de login

--- clinic_management/views/loginView.php
+++ clinic_management/views/loginView.php
@@ -5,6 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="../public/img/favicon.ico">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+  <!-- Bootstrap Icons CDN -->
   <link rel="stylesheet" href="/clinic_management/public/styles/autentication/login.css">
   <link rel="stylesheet" href="/clinic_management/public/styles/global/geral.css">
   <title>Vitta Clinic - Login</title>
@@ -13,7 +15,6 @@
 <body>
 
   <main class="login-container">
-    <!-- Bloco de vídeo -->
     <section class="video-container">
       <video autoplay muted loop>
         <source src="../public/img/video-login.mp4" type="video/mp4">
@@ -22,17 +23,18 @@
       <p class="info-video">©Direitos reservados - Vitta Clinic 2024</p>
     </section>
 
-    <!-- Formulário de login -->
     <section class="form-container">
       <div class="top-form-container-logo">
-        <img src="../public/img/vitta-logo-header.svg" alt="logo" />
+        <a href="/clinic_management/views/landingPageView.php">
+          <img src="../public/img/vitta-logo-header.svg" alt="logo" />
+        </a>
       </div>
       <div class="form-inner-container">
         <h1 class="sign-in-title poppins-medium c11">Entre na sua conta</h1>
         <form method="post" action="/clinic_management/auth/login.php">
           <select id="user_type" name="user_type" required>
-            <option value="clinica">Clínica</option>
-            <option value="admin">Admin</option>
+            <option value="clinica">Clínica Gestora</option>
+            <option value="admin">Administrador</option>
             <option value="medico">Médico</option>
             <option value="paciente">Paciente</option>
           </select>
@@ -45,15 +47,57 @@
             <label class="roboto-regular" for="user_password">Senha <strong class="danger">*</strong></label>
             <input type="password" id="user_password" class="roboto-regular" name="user_password"
               placeholder="**********" required>
+            <i class="bi bi-eye-slash eye-icon" id="togglePassword"></i>
           </div>
           <button type="submit" class="btn btn-primary">Entrar</button>
         </form>
-        <div class="back-to-dashboard">
-          <a href="/clinic_management/views/landingPageView.php">Voltar para o portal</a>
+
+        <div class="forgot-password">
+          <i class="bi bi-info-circle"></i>
+          <span>Esqueceu a senha? <strong class="strong-primary">
+              Entre em contato com a clínica gestora.
+            </strong></span>
         </div>
       </div>
+      <div class="back-to-dashboard" style="position: absolute; bottom: 20px; text-align: center;">
+        <a href="/clinic_management/views/landingPageView.php">
+          <i class="bi bi-arrow-left"></i>
+          Voltar para o portal
+        </a>
+      </div>
     </section>
+
+
   </main>
+
+  <script>
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('user_password');
+
+    function toggleEyeIcon() {
+      if (passwordInput.value.length > 0) {
+        togglePassword.style.display = 'block';
+      } else {
+        togglePassword.style.display = 'none';
+      }
+    }
+
+    toggleEyeIcon();
+
+    passwordInput.addEventListener('input', toggleEyeIcon);
+
+    togglePassword.addEventListener('click', function () {
+      const type = passwordInput.type === 'password' ? 'text' : 'password';
+      passwordInput.type = type;
+      if (type === 'password') {
+        togglePassword.classList.remove('bi-eye');
+        togglePassword.classList.add('bi-eye-slash');
+      } else {
+        togglePassword.classList.remove('bi-eye-slash');
+        togglePassword.classList.add('bi-eye');
+      }
+    });
+  </script>
 
 </body>
 
